8e88a6021ff6ebe0a29d84dbc5ac42e0e8a59b93
Sam-92 <sanchit4u2001@gmail.com>
2020-09-25 13:10:43 +0530

    add 1420D

--- CONTESTS/CODEFORCES/Cf_contest/1420/D_Rescue_Nibel_.cpp
+++ CONTESTS/CODEFORCES/Cf_contest/1420/D_Rescue_Nibel_.cpp
@@ -2,7 +2,7 @@
 using namespace std; 
 
 #define IOS {ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
-#define ll long long 
+#define ll long long
 
 ll powermod(ll x,ll ex,ll md){ll ans=1ll;while(ex>0){if(ex&1ll) ans=(ans*x)%md; ex>>=1ll;x=(x*x)%md;}return ans;}
 inline ll gcd(ll a,ll b) {if(b==0)return a;a%=b;return gcd(b,a);}
@@ -13,9 +13,14 @@ const ll mod = 998244353;
 ll nCr(vector<ll>& fact, ll n,ll r){
         if(n<r) return 0;
         ll p=fact[n]%mod;
+        
         p*=powermod(fact[r],mod-2,mod);
+        p%=mod;
+        
         p*=powermod(fact[n-r],mod-2,mod);
-        return p%mod;
+        p%=mod;
+
+        return p;
 }
 int main(){
         IOS;
@@ -29,8 +34,8 @@ int main(){
         for(ll i=0;i<n;i++){
                 ll x,y;
                 cin>>x>>y;
-                v.push_back(make_pair(x,1));
-                v.push_back(make_pair(y,-1));
+                v.push_back(make_pair(x,-1));
+                v.push_back(make_pair(y,1));
         }
         sort(v.begin() , v.end());
 
@@ -41,7 +46,7 @@ int main(){
 
         ll active=0,ans=0;
         for(ll i=0;i<n+n;i++){
-                if(v[i].second==1){
+                if(v[i].second==-1){
                         ans+=nCr(fact,active,k-1);
                         ans%=mod;
 

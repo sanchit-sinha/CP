4037b96d9a0dca9d7c9f42a7b45c839ca0590760
Sam-92 <sanchit4u2001@gmail.com>
2020-09-25 13:14:59 +0530

    update

--- CONTESTS/CODEFORCES/Cf_contest/1420/B_Rock_and_Lever.cpp
+++ CONTESTS/CODEFORCES/Cf_contest/1420/B_Rock_and_Lever.cpp
@@ -19,39 +19,6 @@ int main(){
                 cin>>n;
                 ll a[n];
                 for(ll i=0;i<n;i++) cin>>a[i];
-                // vector<vector<ll>> ct(n+1 , vector<ll>(31 , 0));
-                // ll i=0;
-                // for(ll j=0;j<31;j++){
-                //         if(a[i]&(1ll<<j)) ct[i][j]=1;
-                // }
-                // for(ll i=1;i<n;i++){
-                //         for(ll j=0;j<31;j++){
-                //                 if(a[i]&(1ll<<j)) ct[i][j]=ct[i-1][j]+1;
-                //                 else ct[i][j]=ct[i-1][j];
-                //         }
-                // }
-
-                // for(ll i=0;i<n;i++){
-                //         for(ll j=0;j<4;j++) cout<<ct[i][j]<<" ";
-                //         cout<<"\n";
-                // }
-                // cout<<"\n";
-                // ll ans=0;
-                // for(ll i=0;i<n;i++){
-                //         ll index=-1;
-                //         for(ll j=30;j>=0;j--){
-                //                 if(a[i]&(1ll<<j)){
-                //                         index=j;
-                //                         break;
-                //                 }
-                //         }
-                //         if(index==-1) continue;
-                //         else{
-                //                 ans+=(ct[n-1][index] - ct[i][index]);
-                //         }
-                // }
-                // cout<<ans<<"\n";
-
                 map<ll,ll>m;
                 for(ll i=0;i<n;i++){
                         for(ll j=31;j>=0;j--){

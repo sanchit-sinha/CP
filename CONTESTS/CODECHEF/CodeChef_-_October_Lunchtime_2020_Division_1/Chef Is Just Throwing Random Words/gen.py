1225d8902d5ef574fc1979bb130462db23cbe153
Sam-92 <sanchit4u2001@gmail.com>
2020-11-01 11:09:42 +0530

    Add lunchtime

---
+++ CONTESTS/CODECHEF/CodeChef_-_October_Lunchtime_2020_Division_1/Chef Is Just Throwing Random Words/gen.py
@@ -0,0 +1,8 @@
+import random
+
+n=(random.randint(1, 3))
+print(n);
+
+for i in range(n):
+	x=(random.randint(1, 5))
+	print(x)
---
+++ CONTESTS/CODEFORCES/Cf_contest/Codeforces_-_DP/B. Sweets Eating/sol.cpp
@@ -0,0 +1,74 @@
+/**
+ *    Author:  Sanchit Sinha
+ *    Created: 31.10.2020 11:32:12       
+**/
+#include "bits/stdc++.h"
+using namespace std;
+#define IOS {ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
+#define pb push_back
+#define nl cout<<"\n"
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define all(c) (c).begin(),(c).end()
+#define sz(c) (int)(c.size())
+#define mp make_pair
+#define mt make_tuple
+#define get(x,c) get<x>(c)
+#define trav(a,x) for(auto a=x.begin();a!=x.end();a++)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define FORR(i,b,a) for(int i=b;i>=a;i--)
+#define lb lower_bound
+#define ub upper_bound
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define f first
+#define s second
+
+inline ll gcd(ll a,ll b) {if(b==0)return a;a%=b;return gcd(b,a);}
+inline ll max(ll a,ll b) {return((a>b)?a:b);}
+inline ll min(ll a,ll b) {return((a>b)?b:a);}
+ll power(ll x,ll y);
+ll powermod(ll x,ll ex,ll md);
+
+const ll inf = 1e18 + 9;
+const ll mod = 1e9 + 7;
+const ld PI = acos(-1);
+const ld eps = 1e-9;
+
+const ll N = 2e5+11;
+ll n,m,a[N],mpfx[N];
+ll dp[N];
+void solve(){
+    cin>>n>>m;
+    FOR(i,1,n) cin>>a[i];
+    sort(a+1,a+n+1);
+    FOR(i,1,n){
+        mpfx[i]=a[i];
+        if(i>m) mpfx[i]+=mpfx[i-m];
+    }
+    FOR(i,1,n){
+        if(i<=m) dp[i]=dp[i-1]+a[i];
+        else dp[i]=dp[i-1]+mpfx[i];
+    }
+    FOR(i,1,n) cout<<dp[i]<<" ";
+    cout<<"\n";
+}
+int main(){
+    IOS;
+    cout<<fixed<<setprecision(20);
+    ll NTC=1;
+    // cin>>NTC;
+    ll PTC=0;
+    while((PTC++)<NTC){
+        // cout<<"Case #"<<PTC<<":"<<' ';
+        solve();
+        //cout<<"\n";
+    }
+    cerr<<"Time : "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+}
+
+ll power(ll x,ll y){if(y==0) return 1;ll a=power(x,y/2);if(y%2==0) return a*a;else return x*a*a;}
+ll powermod(ll x,ll ex,ll md){ll ans=1ll;while(ex>0){if(ex&1ll) ans=(ans*x)%md; ex>>=1ll;x=(x*x)%md;}return ans;}

5be4dcf15c0ab21982b6d150b74798f316e9e40e
Sanchit Sinha <68417985+sanchit-sinha@users.noreply.github.com>
2020-12-02 16:54:32 +0530

    Add Unofficial DIv4 Rounds

---
+++ CONTESTS/CODEFORCES/Cf_contest/Unofficial-Div-4-Round-#1/D/sol.cpp
@@ -0,0 +1,119 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define IOS {ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
+#define pb push_back
+#define ll long long
+#define ld long double
+#define all(c) (c).begin(),(c).end()
+#define sz(c) (int)(c.size())
+#define mp make_pair
+#define mt make_tuple
+#define get(x,c) get<x>(c)
+#define trav(a,x) for(auto a=x.begin();a!=x.end();a++)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define FORR(i,b,a) for(int i=b;i>=a;i--)
+#define lb lower_bound
+#define ub upper_bound
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define f first
+#define s second
+
+inline ll gcd(ll a,ll b) {if(b==0)return a;a%=b;return gcd(b,a);}
+inline ll max(ll a,ll b) {return((a>b)?a:b);}
+inline ll min(ll a,ll b) {return((a>b)?b:a);}
+ll power(ll x,ll ex);
+ll powermod(ll x,ll ex,ll md);
+
+const ll inf = 1e18 + 9;
+const ll mod = 1e9 + 7;
+const ld PI = acos(-1);
+const ld eps = 1e-9;
+
+const ll N = 1e5+11;
+
+// finding shortest prime factor//SPF -- Nlog(n)
+const ll mX = 1e2 + 11;
+ll spf[mX];
+void sieve (){
+    spf[1]=1;
+    for(ll i=2;i<mX;i++){
+        if(spf[i]==0){
+            spf[i]=i;
+            for (ll j=1ll*i*i;j<mX;j+=i){
+                if(spf[j]==0) spf[j]=i;
+            }
+        }
+    }
+}
+bool checkone(ll a[],ll n){
+	ll ct =0 ;
+	rep(i, n)if(a[i] == 1) ct++;
+
+	if(ct > 1) return true;
+	else return false;
+}
+void solve(){
+	 ll n , k;
+	 cin >> n >> k;
+
+	 ll a[n];
+	 map<ll , ll > m;
+
+	 bool one = 0;
+
+	 rep(i, n){
+	 	cin >> a[i];
+	 	if(a[i] == 1) one = 1;
+	 	m[spf[a[i]]]++;
+	 }
+
+
+
+	ll len = sz(m);
+	if(len == 1){
+		if(k%2 == 1){
+			if(checkone(a,n)) cout <<"NO\n";
+			else cout <<"YES\n";
+		}
+		else cout << "NO\n";
+	}
+	else if(len == 2){
+		vll ct;
+		trav(it , m) ct.pb(it->s);
+
+		sort(all(ct));
+		ll mn = ct[0];
+
+		if(mn != 1){
+			cout <<"NO\n";
+		}
+		else{
+			if(k%2 == 1){
+				if(checkone(a, n)) cout <<"NO\n";
+				else cout <<"YES\n";
+			}
+			else cout <<"NO\n";
+		}
+	}
+	else cout <<"NO\n";
+}	
+
+int main(){
+    IOS;
+    cout<<fixed<<setprecision(20);
+    ll NTC=1;
+    // cin>>NTC;
+    ll PTC=0;
+    while((PTC++)<NTC){
+        // cout<<"Case #"<<PTC<<":"<<' ';
+        solve();
+        //cout<<"\n";
+    }
+    //cerr<<"Time : "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+}
+
+ll power(ll x,ll y){if(y==0) return 1;ll a=power(x,y/2);if(y%2==0) return a*a;else return x*a*a;}
+ll powermod(ll x,ll ex,ll md){ll ans=1ll;while(ex>0){if(ex&1ll) ans=(ans*x)%md; ex>>=1ll;x=(x*x)%md;}return ans;}

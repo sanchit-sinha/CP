129b353e5918594dc9a1650bba0daa568c863132
Sanchit Sinha <68417985+sanchit-sinha@users.noreply.github.com>
2020-12-22 13:09:51 +0530

    Add files via upload

---
+++ CONTESTS/CODEFORCES/Cf_contest/CF1462/C/sol.cpp
@@ -0,0 +1,92 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define IOS {ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
+#define pb push_back
+#define ll long long
+#define ld long double
+#define all(c) (c).begin(),(c).end()
+#define sz(c) (int)(c.size())
+#define mp make_pair
+#define mt make_tuple
+#define get(x,c) get<x>(c)
+#define trav(a,x) for(auto a=x.begin();a!=x.end();a++)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define FORR(i,b,a) for(int i=b;i>=a;i--)
+#define lb lower_bound
+#define ub upper_bound
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define f first
+#define s second
+
+inline ll gcd(ll a,ll b) {if(b==0)return a;a%=b;return gcd(b,a);}
+inline ll max(ll a,ll b) {return((a>b)?a:b);}
+inline ll min(ll a,ll b) {return((a>b)?b:a);}
+ll power(ll x,ll ex);
+ll powermod(ll x,ll ex,ll md);
+
+const ll inf = 1e18 + 9;
+const ll mod = 1e9 + 7;
+const ld PI = acos(-1);
+const ld eps = 1e-9;
+
+const ll N = 1e5+11;
+ll check(ll x,ll n){
+	ll a[11] = {0};
+	ll index = 0;
+	ll temp = x;
+	while(temp){
+		a[index++] = temp%2;
+		temp/=2;
+	}
+	ll sum = 0;
+	FOR(i , 1 , 9){
+		if(a[i - 1] == 1) sum += i;
+	}
+
+	if(sum == n){
+		ll num = 0;
+		FOR(i ,1 ,9){
+			if(a[i - 1] == 1){
+				num = num*10 + i;
+			}
+		}
+		return num;
+	}
+	else return -1;
+
+}
+
+void solve(){
+    ll n;
+    cin >> n;
+    ll bit = (1ll<<9);
+    ll ans = LLONG_MAX;
+    FOR(i , 1 , bit){
+    	ll x = check(i, n);
+    	if(x != -1) ans = min(ans , x);
+    } 
+
+    if(ans != LLONG_MAX) cout << ans <<"\n";
+    else cout << -1 << "\n";
+}
+
+int main(){
+    IOS;
+    cout<<fixed<<setprecision(20);
+    ll NTC=1;
+    cin>>NTC;
+    ll PTC=0;
+    while((PTC++)<NTC){
+        // cout<<"Case #"<<PTC<<":"<<' ';
+        solve();
+        //cout<<"\n";
+    }
+    //cerr<<"Time : "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+}
+
+ll power(ll x,ll y){if(y==0) return 1;ll a=power(x,y/2);if(y%2==0) return a*a;else return x*a*a;}
+ll powermod(ll x,ll ex,ll md){ll ans=1ll;while(ex>0){if(ex&1ll) ans=(ans*x)%md; ex>>=1ll;x=(x*x)%md;}return ans;}
+		

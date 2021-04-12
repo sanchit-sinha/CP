abc4748b0752abe352fcaa02eb11dd5e968fbd69
Sanchit Sinha <68417985+sanchit-sinha@users.noreply.github.com>
2021-04-12 23:43:52 +0530

    upload cf 1513

---
+++ CONTESTS/CODEFORCES/Cf_contest/DIV2-1513/D/sol.cpp
@@ -0,0 +1,113 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define IOS {ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
+#define pb push_back
+#define ll long long
+#define ld long double
+#define all(c) (c).begin(),(c).end()
+#define sz(c) (int)(c.size())
+#define mp make_pair
+#define mt make_tuple
+#define get(x,c) get<x>(c)
+#define trav(a,x) for(auto a=x.begin();a!=x.end();a++)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define FORR(i,b,a) for(int i=b;i>=a;i--)
+#define lb lower_bound
+#define ub upper_bound
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define f first
+#define s second
+
+inline ll gcd(ll a,ll b) {if(b==0)return a;a%=b;return gcd(b,a);}
+inline ll max(ll a,ll b) {return((a>b)?a:b);}
+inline ll min(ll a,ll b) {return((a>b)?b:a);}
+ll power(ll x,ll ex);
+ll powermod(ll x,ll ex,ll md);
+
+const ll inf = 1e18 + 9;
+const ll mod = 1e9 + 7;
+const ld PI = acos(-1);
+const ld eps = 1e-9;
+
+const ll N = 2e5+11;
+
+ll n , p;
+ll a[N];
+vpll v;
+void solve(){
+    v.clear();
+    rep(i , n + 1){
+        a[i] = 0;
+    }
+
+    cin >> n >> p;
+    rep(i , n) cin >> a[i + 1];
+
+    rep(i , n){
+        v.pb(mp(a[i + 1] , i + 1));
+    }
+    sort(all(v));
+    
+    ll ans = 0;
+    ll totComponent = n;
+    bool vis[n + 11] = {0};
+    rep(j , sz(v)){
+        pll mn = v[j];
+        ll index = mn.s;
+        ll ele = mn.f;
+
+        ll ct = 1;
+        if(vis[index]) continue;
+        if(ele < p){
+            FOR(i , index + 1 , n){
+                if(a[i]%ele == 0 && !vis[i]){
+                    vis[i - 1] = 1;
+                    ct++;
+                }
+                else break;
+            }
+            FORR(i , index - 1 , 1){
+                if(a[i]%ele == 0 && !vis[i]){
+                    vis[i + 1] = 1;
+                    ct++;
+                }
+                else break;
+            }
+
+            // cout << index << " : " << ele << " " << ct << "\n";
+            ans += (ct - 1)*ele;
+            totComponent = totComponent - ct + 1;
+        }
+        else break;
+    }
+
+    // cout << "totComponent" << " " << totComponent << "\n\n";
+    ans += (totComponent - 1ll) * p;
+    cout << ans << "\n";
+}
+
+int main(){
+    IOS;
+    cout<<fixed<<setprecision(20);
+    #ifdef SIEVE
+        sieve();
+    #endif
+    #ifdef NCR
+        factorial();
+    #endif
+    ll NTC=1;
+    cin>>NTC;
+    ll PTC=0;
+    while((PTC++)<NTC){
+        // cout<<"Case #"<<PTC<<":"<<' ';
+        solve();
+        //cout<<"\n";
+    }
+    //cerr<<"Time : "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+}
+
+ll power(ll x,ll y){if(y==0) return 1;ll a=power(x,y/2);if(y%2==0) return a*a;else return x*a*a;}
+ll powermod(ll x,ll ex,ll md){ll ans=1ll;while(ex>0){if(ex&1ll) ans=(ans*x)%md; ex>>=1ll;x=(x*x)%md;}return ans;}

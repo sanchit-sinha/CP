51f2cf4e94741e61f28cfaf6fd4dc8c7696cb4a6
Sanchit Sinha <68417985+sanchit-sinha@users.noreply.github.com>
2021-06-12 19:44:46 +0530

    Update sol.cpp

--- Learning+Practice/EDU/TwoPointerC/F/sol.cpp
+++ Learning+Practice/EDU/TwoPointerC/F/sol.cpp
@@ -21,18 +21,18 @@ using namespace std;
 #define FOR(i, a, b)        for(int i = (a); i <= (b); i++)
 #define FORR(i, b, a)       for(int i = (b); i >= (a); i--)
 
-string to_string(string s) {return '"' + s + '"';}string to_string(const char* s) {return to_string((string) s);}string to_string(bool b) {return (b ? "true" : "false");}template <typename A, typename B>string to_string(pair<A, B> p) {return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";}template <typename A>string to_string(A v) {bool first = true;string res = "{";for (const auto &x : v) {if (!first) {res += ", ";}first = false;res += to_string(x);}res += "}";return res;}void debug_out() { cerr << "\n"; }template <typename Head, typename... Tail>void debug_out(Head H, Tail... T) {cerr << " " << to_string(H) << "\n";debug_out(T...);} 
+string to_string(string s) {return '"' + s + '"';} string to_string(const char* s) {return to_string((string) s);} string to_string(bool b) {return (b ? "true" : "false");} template <typename A, typename B>string to_string(pair<A, B> p) {return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";} template <typename A>string to_string(A v) {bool first = true; string res = "{"; for (const auto &x : v) {if (!first) {res += ", ";} first = false; res += to_string(x);} res += "}"; return res;} void debug_out() { cerr << "\n"; } template <typename Head, typename... Tail>void debug_out(Head H, Tail... T) {cerr << " " << to_string(H) << "\n"; debug_out(T...);}
 #ifdef LOCAL
 #define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
 #else
 #define debug(...) 42
 #endif
 
-inline ll gcd(ll a,ll b) {if(b == 0) return a; a %= b; return gcd(b , a);}
-inline ll max(ll a,ll b) {return((a > b) ? a : b);}
-inline ll min(ll a,ll b) {return((a > b) ? b : a);}
-ll power(ll x,ll ex);
-ll powermod(ll x,ll ex,ll md);
+inline ll gcd(ll a, ll b) {if (b == 0) return a; a %= b; return gcd(b , a);}
+inline ll max(ll a, ll b) {return ((a > b) ? a : b);}
+inline ll min(ll a, ll b) {return ((a > b) ? b : a);}
+ll power(ll x, ll ex);
+ll powermod(ll x, ll ex, ll md);
 
 const ll inf = 1e18 + 9;
 const ll mod = 1e9 + 7;
@@ -40,36 +40,63 @@ const ld PI = acos(-1);
 const ld eps = 1e-9;
 
 
-void solve(){
-     
+void solve() {
+    int n , m;
+    cin >> n >> m;
+
+    string s , t;
+    cin >> s >> t;
+
+    vector<int> count(26 , 0) , prescount(26 , 0);
+    for(int i = 0 ; i < m ; i++) count[t[i] - 'a']++;
+
+    int l = 0 , r = 0;
+    ll ans = 0ll;
+    prescount[s[0] - 'a'] = 1;
+    while(l < n && r < n){  
+        bool ok = 1;
+        for(int i = 0 ; i < 26 ; i++) if(prescount[i] > count[i]) ok = 0;
+
+        if(ok){ // all less 
+            ans += 1ll * (r - l + 1);
+            r++;
+            if(r < n) prescount[s[r] - 'a']++;
+        }
+        else{ // more 
+            prescount[s[l] - 'a']--;
+            l++;
+        }
+    }
+
+    cout << ans << "\n";
 }
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     cout << fixed << setprecision(20);
-   
-    ll NTC=1;
-    // cin>>NTC;
 
-    #ifdef SIEVE
-        sieve();
-    #endif
-    #ifdef NCR
-        factorial();
-    #endif 
-    #ifdef PREPROCESS
-        preprocess();
-    #endif
-    
-    ll PTC=0;
-    while((PTC++)<NTC){
+    ll NTC = 1;
+    // cin >> NTC;
+
+#ifdef SIEVE
+    sieve();
+#endif
+#ifdef NCR
+    factorial();
+#endif
+#ifdef PREPROCESS
+    preprocess();
+#endif
+
+    ll PTC = 0;
+    while ((PTC++) < NTC) {
         // cout << "Case #" << PTC << ":" << ' ';
         solve();
-        //cout<<"\n";
+        // cout << "\n";
     }
-    //cerr<<"Time : "<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n";
+    // cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
 }
 
-ll power(ll x,ll y){if(y==0) return 1;ll a=power(x,y/2);if(y%2==0) return a*a;else return x*a*a;}
-ll powermod(ll x,ll ex,ll md){ll ans=1ll;while(ex>0){if(ex&1ll) ans=(ans*x)%md; ex>>=1ll;x=(x*x)%md;}return ans;}
+ll power(ll x, ll y) {if (y == 0) return 1; ll a = power(x, y / 2); if (y % 2 == 0) return a * a; else return x * a * a;}
+ll powermod(ll x, ll ex, ll md) {ll ans = 1ll; while (ex > 0) {if (ex & 1ll) ans = (ans * x) % md; ex >>= 1ll; x = (x * x) % md;} return ans;}

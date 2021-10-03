e07e5ae5fbeddccabd7fc31c1378f28e2ef1208f
Sanchit Sinha <68417985+sanchit-sinha@users.noreply.github.com>
2021-10-03 19:44:43 +0530

    add cf1476

---
+++ Contests/Codeforces/CF-Contest/1476/C/sol.cpp
@@ -0,0 +1,106 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define f                   first
+#define s                   second
+#define pb                  push_back
+#define ll                  long long
+#define ld                  long double
+#define lb                  lower_bound
+#define ub                  upper_bound
+#define mp                  make_pair
+#define mt                  make_tuple
+#define pll                 pair<ll , ll>
+#define vll                 vector<ll>
+#define vvll                vector<vll>
+#define vpll                vector<pll>
+#define all(c)              (c).begin(),(c).end()
+#define sz(c)               (ll)((c).size())
+#define get(x,c)            get<x>(c)
+#define trav(a,x)           for(auto a = x.begin() ; a != x.end() ; a++)
+#define rep(i, n)           for(ll i = 0; i < (n) ; i++)
+#define FOR(i, a, b)        for(ll i = (a); i <= (b); i++)
+#define FORR(i, b, a)       for(ll i = (b); i >= (a); i--)
+
+#ifdef LOCAL
+#include "debug.cpp"
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]: ", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#define cerr if(false) cerr
+#endif
+
+
+inline ll gcd(ll a, ll b) {if (b == 0) return a; a %= b; return gcd(b , a);}
+inline ll max(ll a, ll b) {return ((a > b) ? a : b);}
+inline ll min(ll a, ll b) {return ((a > b) ? b : a);}
+ll power(ll x, ll ex);
+ll powermod(ll x, ll ex, ll md);
+
+const ll inf = 4e18 + 9;
+const ll mod = 1e9 + 7;
+const ld PI = acos(-1);
+const ld eps = 1e-9;
+
+
+void solve() {
+    ll n;
+    cin >> n;
+
+    vll c(n);
+    rep(i, n) cin >> c[i];
+
+    vll a(n), b(n);
+    rep(i, n) cin >> a[i];
+    rep(i, n) cin >> b[i];
+
+    vll ans(n, 0); // ans till ith graph and extending backwards
+
+    debug(n);
+    debug(c);
+    debug(a);
+    debug(b);
+
+    FOR(i, 1, n - 1) {
+        if (i > 1 && a[i] != b[i]) ans[i] = max(ans[i], ans[i - 1] - abs(a[i] - b[i]) + 2 + c[i] - 1); // move backs back
+        ans[i] = max(ans[i], 2 + c[i] - 1 + abs(a[i] - b[i])); // consider only with back
+    }
+
+    debug(ans);
+    ll val = 0ll;
+    rep(i, n) val = max(val, ans[i]);
+    cout << val << "\n";
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("../error.txt", "w", stderr);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cout << fixed << setprecision(20);
+
+    ll NTC = 1;
+    cin >> NTC;
+
+#ifdef PREPROCESS
+    preprocess();
+#endif
+#ifdef SIEVE
+    sieve();
+#endif
+#ifdef NCR
+    factorial();
+#endif
+
+    ll PTC = 0;
+    while ((PTC++) < NTC) {
+        cerr << "Testcase # " << PTC << "\n";
+        // cout << "Case #" << PTC << ":" << ' ';
+        solve();
+        cerr << "*************************\n";
+    }
+    // cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+
+ll power(ll x, ll y) {if (y == 0) return 1; ll a = power(x, y / 2); if (y % 2 == 0) return a * a; else return x * a * a;}
+ll powermod(ll x, ll ex, ll md) {ll ans = 1ll; while (ex > 0) {if (ex & 1ll) ans = (ans * x) % md; ex >>= 1ll; x = (x * x) % md;} return ans;}

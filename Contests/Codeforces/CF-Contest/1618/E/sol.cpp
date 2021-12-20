39a2c1aa0f4729133dd7102a6a8f5261797c603c
Sanchit Sinha <68417985+sanchit-sinha@users.noreply.github.com>
2021-12-20 15:28:19 +0530

    add 1618

---
+++ Contests/Codeforces/CF-Contest/1618/E/sol.cpp
@@ -0,0 +1,121 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define f                   first
+#define s                   second
+#define pb                  push_back
+#define ll                  long long
+#define ld                  long double
+#define lb                  lower_bound
+#define ub                  upper_bound
+#define mp                  make_pair
+#define mt                  make_tuple
+#define pll                 pair<ll, ll>
+#define vll                 vector<ll>
+#define vvll                vector<vll>
+#define vpll                vector<pll>
+#define all(c)              (c).begin(),(c).end()
+#define sz(c)               (ll)((c).size())
+#define get(x,c)            get<x>(c)
+#define trav(a,x)           for(auto a = x.begin(); a != x.end(); a++)
+#define rep(i, n)           for(ll i = 0; i < (n) ; i++)
+#define FOR(i, a, b)        for(ll i = (a); i <= (b); i++)
+#define FORR(i, b, a)       for(ll i = (b); i >= (a); i--)
+
+#ifdef LOCAL
+bool DEBUG = 1;
+#include "debug.cpp"
+#else
+bool DEBUG = 0;
+#define debug(...) 42
+#define cerr if(false) cerr
+#endif
+
+inline ll gcd(ll a, ll b) {if (b == 0) return a; a %= b; return gcd(b , a);}
+inline ll max(ll a, ll b) {return ((a > b) ? a : b);}
+inline ll min(ll a, ll b) {return ((a > b) ? b : a);}
+ll power(ll x, ll ex);
+ll powermod(ll x, ll ex, ll md);
+
+const ll inf = 1e9;
+const ll mod = 1e9 + 7;
+const ld PI = acos(-1);
+const ld eps = 1e-9;
+
+
+void solve() {
+    ll n;
+    cin >> n;
+
+    vll b(n), a(n);
+    rep(i, n) cin >> b[i];
+
+    ll sum = 0ll, sigma = 0ll, suma = 0ll;
+    rep(i, n) sum += b[i];
+
+    ll div = 1ll * (n * (n + 1)) / 2;
+    if (sum % div != 0) {
+        cout << "NO\n";
+        return;
+    }
+
+    sigma = sum / div;
+    FOR(i, 1, n - 1) {
+        ll dif = b[i] - b[i - 1];
+        ll nai = sigma - dif;
+
+        if (nai % n != 0) {
+            cout << "NO\n";
+            return;
+        }
+
+        a[i] = nai / n;
+        suma += a[i];
+    }
+
+    a[0] = sigma - suma;
+    rep(i, n) {
+        if (a[i] >= 1 && a[i] <= inf) continue;
+        else {
+            cout << "NO\n";
+            return;
+        }
+    }
+
+    cout << "YES\n";
+    rep(i, n) cout << a[i] << " ";
+    cout << "\n";
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("../error.txt", "w", stderr);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cout << fixed << setprecision(20);
+
+    ll NTC = 1;
+    cin >> NTC;
+
+#ifdef PREPROCESS
+    preprocess();
+#endif
+#ifdef SIEVE
+    sieve();
+#endif
+#ifdef NCR
+    factorial();
+#endif
+
+    ll PTC = 0;
+    while ((PTC++) < NTC) {
+        cerr << "Testcase # " << PTC << "\n";
+        // cout << "Case #" << PTC << ":" << ' ';
+        solve();
+        cerr << "*************************\n";
+    }
+    // cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+
+ll power(ll x, ll y) {if (y == 0) return 1; ll a = power(x, y / 2); if (y % 2 == 0) return a * a; else return x * a * a;}
+ll powermod(ll x, ll ex, ll md) {ll ans = 1ll; while (ex > 0) {if (ex & 1ll) ans = (ans * x) % md; ex >>= 1ll; x = (x * x) % md;} return ans;}

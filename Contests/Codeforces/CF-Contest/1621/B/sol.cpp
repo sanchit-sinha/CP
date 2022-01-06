359de37ae7236454eac4b5848fe0680cd324c988
Sanchit Sinha <68417985+sanchit-sinha@users.noreply.github.com>
2022-01-06 14:40:41 +0530

    add 1621

---
+++ Contests/Codeforces/CF-Contest/1621/B/sol.cpp
@@ -0,0 +1,117 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define f                   first
+#define s                   second
+#define pb                  push_back
+#define ll                  long long
+#define ld                  long double
+#define lb                  lower_bound
+#define ub                  upper_bound
+#define mp                  make_pair
+#define mt                  make_tuple
+#define pll                 pair<ll, ll>
+#define vll                 vector<ll>
+#define vvll                vector<vll>
+#define vpll                vector<pll>
+#define all(c)              (c).begin(),(c).end()
+#define sz(c)               (ll)((c).size())
+#define get(x,c)            get<x>(c)
+#define trav(a,x)           for(auto a = x.begin(); a != x.end(); a++)
+#define rep(i, n)           for(ll i = 0; i < (n) ; i++)
+#define FOR(i, a, b)        for(ll i = (a); i <= (b); i++)
+#define FORR(i, b, a)       for(ll i = (b); i >= (a); i--)
+
+#ifdef LOCAL
+bool DEBUG = 1;
+#include "debug.cpp"
+#else
+bool DEBUG = 0;
+#define debug(...) 42
+#define cerr if(false) cerr
+#endif
+
+inline ll gcd(ll a, ll b) {if (b == 0) return a; a %= b; return gcd(b , a);}
+inline ll max(ll a, ll b) {return ((a > b) ? a : b);}
+inline ll min(ll a, ll b) {return ((a > b) ? b : a);}
+ll power(ll x, ll ex);
+ll powermod(ll x, ll ex, ll md);
+
+const ll inf = 4e18 + 9;
+const ll mod = 1e9 + 7;
+const ld PI = acos(-1);
+const ld eps = 1e-9;
+
+
+void solve() {
+    ll n;
+    cin >> n;
+
+    map<ll, set<pll>> segments;
+    map<ll, set<ll>> cost;
+    rep(i, n) {
+        ll l, r, c;
+        cin >> l >> r >> c;
+
+        cost[l].insert(c);
+        cost[r].insert(c);
+        segments[l].insert({r, c});
+
+        ll mn = cost.begin()->f;
+        ll mx = cost.rbegin()->f;
+
+        ll ans1 = *cost[mn].begin() + *cost[mx].begin();
+        ll ans = ans1;
+
+        debug(i, mn, mx);
+        if (DEBUG) {
+            trav(it, segments) {
+                debug(it->f);
+                debug(it->s);
+            }
+        }
+
+        auto it = segments[mn].lb({mx, -inf});
+        if (it != segments[mn].end()) {
+            if (it->f == mx) {
+                cerr << " in 2" << it->f << " : " << it->s << "\n";
+                ans = min(ans, it->s);
+            }
+        }
+
+        cout << ans << "\n";
+    }
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("../error.txt", "w", stderr);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cout << fixed << setprecision(20);
+
+    ll NTC = 1;
+    cin >> NTC;
+
+#ifdef PREPROCESS
+    preprocess();
+#endif
+#ifdef SIEVE
+    sieve();
+#endif
+#ifdef NCR
+    factorial();
+#endif
+
+    ll PTC = 0;
+    while ((PTC++) < NTC) {
+        cerr << "Testcase # " << PTC << "\n";
+        // cout << "Case #" << PTC << ":" << ' ';
+        solve();
+        cerr << "*************************\n";
+    }
+    // cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+
+ll power(ll x, ll y) {if (y == 0) return 1; ll a = power(x, y / 2); if (y % 2 == 0) return a * a; else return x * a * a;}
+ll powermod(ll x, ll ex, ll md) {ll ans = 1ll; while (ex > 0) {if (ex & 1ll) ans = (ans * x) % md; ex >>= 1ll; x = (x * x) % md;} return ans;}
---
+++ Contests/Codeforces/CF-Contest/1621/D/sol.cpp
@@ -0,0 +1,107 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define f                   first
+#define s                   second
+#define pb                  push_back
+#define ll                  long long
+#define ld                  long double
+#define lb                  lower_bound
+#define ub                  upper_bound
+#define mp                  make_pair
+#define mt                  make_tuple
+#define pll                 pair<ll, ll>
+#define vll                 vector<ll>
+#define vvll                vector<vll>
+#define vpll                vector<pll>
+#define all(c)              (c).begin(),(c).end()
+#define sz(c)               (ll)((c).size())
+#define get(x,c)            get<x>(c)
+#define trav(a,x)           for(auto a = x.begin(); a != x.end(); a++)
+#define rep(i, n)           for(ll i = 0; i < (n) ; i++)
+#define FOR(i, a, b)        for(ll i = (a); i <= (b); i++)
+#define FORR(i, b, a)       for(ll i = (b); i >= (a); i--)
+
+#ifdef LOCAL
+bool DEBUG = 1;
+#include "debug.cpp"
+#else
+bool DEBUG = 0;
+#define debug(...) 42
+#define cerr if(false) cerr
+#endif
+
+inline ll gcd(ll a, ll b) {if (b == 0) return a; a %= b; return gcd(b , a);}
+inline ll max(ll a, ll b) {return ((a > b) ? a : b);}
+inline ll min(ll a, ll b) {return ((a > b) ? b : a);}
+ll power(ll x, ll ex);
+ll powermod(ll x, ll ex, ll md);
+
+const ll inf = 4e18 + 9;
+const ll mod = 1e9 + 7;
+const ld PI = acos(-1);
+const ld eps = 1e-9;
+
+#define plpl pair<ll,pll>
+
+ll dx[4] = {1, -1, 0, 0};
+ll dy[4] = {0, 0, 1, -1};
+void solve() {
+	ll n;
+	cin >> n;
+
+	ll m = n + n;
+	vvll cost(m + 2, vll (m + 2, 0));
+
+	ll ans = 0ll;
+	FOR(i, 1, m) FOR(j, 1, m) cin >> cost[i][j];
+	FOR(i, 1, m) FOR(j, 1, m) if (i >= n + 1 && j >= n + 1) ans += cost[i][j], cost[i][j] = 0ll;
+
+	ll anss = inf;
+	anss = min(anss, cost[n + 1][1]);
+	anss = min(anss, cost[1][n + 1]);
+
+	anss = min(anss, cost[n + 1][n]);
+	anss = min(anss, cost[n][n + 1]);
+
+	anss = min(anss, cost[m][1]);
+	anss = min(anss, cost[1][m]);
+
+	anss = min(anss, cost[m][n]);
+	anss = min(anss, cost[n][m]);
+
+	cout << ans + anss << "\n";
+}
+
+int main() {
+#ifdef LOCAL
+	freopen("../error.txt", "w", stderr);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	cout << fixed << setprecision(20);
+
+	ll NTC = 1;
+	cin >> NTC;
+
+#ifdef PREPROCESS
+	preprocess();
+#endif
+#ifdef SIEVE
+	sieve();
+#endif
+#ifdef NCR
+	factorial();
+#endif
+
+	ll PTC = 0;
+	while ((PTC++) < NTC) {
+		cerr << "Testcase # " << PTC << "\n";
+		// cout << "Case #" << PTC << ":" << ' ';
+		solve();
+		cerr << "*************************\n";
+	}
+	// cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+
+ll power(ll x, ll y) {if (y == 0) return 1; ll a = power(x, y / 2); if (y % 2 == 0) return a * a; else return x * a * a;}
+ll powermod(ll x, ll ex, ll md) {ll ans = 1ll; while (ex > 0) {if (ex & 1ll) ans = (ans * x) % md; ex >>= 1ll; x = (x * x) % md;} return ans;}
